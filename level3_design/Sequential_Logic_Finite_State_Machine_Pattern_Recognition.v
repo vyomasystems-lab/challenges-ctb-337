5ed39d2d2cc457991f7f758b3fd12cae65a02c2f
ctb-337 <109905598+ctb-337@users.noreply.github.com>
2022-07-25 21:46:59 +0530

    Update Sequential_Logic_Finite_State_Machine_Pattern_Recognition.v

--- level3_design/Sequential_Logic_Finite_State_Machine_Pattern_Recognition.v
+++ level3_design/Sequential_Logic_Finite_State_Machine_Pattern_Recognition.v
@@ -9,9 +9,9 @@ module pattern_recognition(
 reg [2:0] current_state, next_state;
 
 parameter [2:0] S0      = 3'b000;
-parameter [2:0] S10     = 3'b001;
-parameter [2:0] S110    = 3'b010;
-parameter [2:0] S0110   = 3'b011;
+parameter [2:0] S00     = 3'b001;
+parameter [2:0] S001    = 3'b010;
+parameter [2:0] S0011   = 3'b011;
 parameter [2:0] S00110  = 3'b100;
 
 assign found = (current_state == S00110) ? 1 : 0;
@@ -29,23 +29,23 @@ end
 always @ (current_state or in) begin
     case (current_state)
         S0: begin
-            if (in) next_state <= S10;
+            if (!in) next_state <= S00;
             else    next_state <= S0;
         end
-        S10: begin
-            if (in) next_state <= S110;
+        S00: begin
+            if (in) next_state <= S001;
             else    next_state <= S0;
         end
-        S110: begin
-            if (!in) next_state <= S0110;
+        S001: begin
+            if (in) next_state <= S0011;
             else    next_state <= S0;
         end
-        S0110: begin
+        S0011: begin
             if (!in) next_state <= S00110;
             else    next_state <= S0;
         end
         S00110:begin
-            if (in) next_state <= S10;
+            if (!in) next_state <= S00;
             else    next_state <= S0;
         end
     endcase
