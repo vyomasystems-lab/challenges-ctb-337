bebff421941e0406662f374eacfe893782502fd8
ctb-337 <109905598+ctb-337@users.noreply.github.com>
2022-07-29 17:50:05 +0530

    Update Sequential_Logic_Finite_State_Machine_Pattern_Recognition.v

--- level3_design/Sequential_Logic_Finite_State_Machine_Pattern_Recognition.v
+++ level3_design/Sequential_Logic_Finite_State_Machine_Pattern_Recognition.v
@@ -1,73 +1,56 @@
-//Sequential logic state machine pattern recognition
-//Recognition pattern_1101
-
-`timescale 1ns /1ps
-
-
-module FSM_Moore_1101(
-input clk,
-input reset,
-input din,
-output reg dout,
-output reg [2:0] present_state
+// Recognize the pattern `00110` in a serial stream
+module pattern_recognition(
+    input        clk,           // clk
+    input        rst,           // Reset
+    input        in,            // Serial in
+    output       found          // Found pattern
 );
 
-// 4 state of the moore machine
-parameter S0=3'b000;      // idle
-parameter S1=3'b001;      // pattern detected 1
-parameter S2=3'b010;      // pattern detected 11
-parameter S3=3'b011;      // pattern detected 110
-parameter S4=3'b100;      // pattern detected 1101
+reg [2:0] current_state, next_state;
 
-    
-reg [2:0] next_state;
+// Doesn't really matter with the bits
+parameter [2:0] S0      = 3'b000;
+parameter [2:0] S10     = 3'b001;
+parameter [2:0] S110    = 3'b010;
+parameter [2:0] S0110   = 3'b011;
+parameter [2:0] S00110  = 3'b100;
 
-    // at every positive edge of clock , present state(present_state) is updated 
-    always@(posedge clk or posedge reset)
-     begin
-       if(reset)
-        present_state<=S0;
-       else
-        present_state<=next_state;
-       end
-	   
-	 // Whenever a new input is taken, next state is updated  
-	   always@(*)
-       begin
-        case(present_state)
-        S0: if(din==1)
-                next_state<=S1;
-            else
-                next_state<=S0;
-         S1: if(din==1)
-                next_state<=S2;
-              else
-                next_state<=S0;
-         S2: if(din==0)
-                next_state<=S3;
-             else
-                next_state<=S2;
-         S3: if(din==1)
-                next_state<=S4;
-              else
-                next_state<=S0;
-         S4: if(din==1)
-                next_state<=S2;
-             else
-                next_state<=S0;
-        default:
-            next_state<=S0;    
-        endcase            
+assign found = (current_state == S00110) ? 1 : 0;
+
+// CURRENT STATE
+always @ (posedge clk) begin
+    if (rst) begin
+        current_state <= S0;
+    end else begin
+        current_state <= next_state;
+    end
+end
+
+// FINITE STATE MACHINE
+always @ (current_state or in) begin
+    case (current_state)
+        S0: begin
+            if (in) next_state <= S10;
+            else    next_state <= S0;
         end
+        S10: begin
+            if (in) next_state <= S110;
+            else    next_state <= S0;
+        end
+        S110: begin
+            if (!in) next_state <= S0110;
+            else    next_state <= S0;
+        end
+        S0110: begin
+            if (!in) next_state <= S00110;
+            else    next_state <= S0;
+        end
+        S00110:begin
+            if (in) next_state <= S10;
+            else    next_state <= S0;
+        end
+    endcase
 
-always@(*)
-begin
-    if(reset)
-        dout=0;
-    else if(present_state==S4)
-        dout=1;
-    else
-        dout=0;
 end
 
 endmodule
